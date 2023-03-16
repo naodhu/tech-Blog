07420f7f6c2eea9f694c0bb4578ceb9962291e4e
Naod Hunde <chris_naod@yahoo.com>
2023-03-16 23:15:10 +1100

    util functionality

---
+++ utils/helpers.js
@@ -0,0 +1,12 @@
+// This module exports several helper functions that can be used to format data for display on the client side.
+// The `format_date` function takes a JavaScript `Date` object and formats it as a string in the format "MM/DD/YYYY".
+// The `format_amount` function takes a number and formats it with commas as thousands separators.
+module.exports = {
+    format_date: (date) => {
+      return date.toLocaleDateString();
+    },
+    format_amount: (amount) => {
+      return parseInt(amount).toLocaleString();
+    },
+  };
+  
