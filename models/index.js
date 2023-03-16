7526f1c17136090f1224836c3459396332b5a7b0
Naod Hunde <chris_naod@yahoo.com>
2023-03-16 23:13:46 +1100

    model files for index and user created

---
+++ models/index.js
@@ -0,0 +1,25 @@
+// Import required models
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
+
+// Define model associations
+User.hasMany(Blog, {
+  foreignKey: 'user_id', // Set foreign key to user_id
+  onDelete: 'CASCADE' // Set onDelete action to CASCADE
+});
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_id' // Set foreign key to user_id
+});
+
+Blog.hasMany(Comment,{
+  foreignKey: 'blog_id' // Set foreign key to blog_id
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id' // Set foreign key to user_id
+});
+
+// Export all models
+module.exports = { User, Blog, Comment };
