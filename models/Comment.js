577f0d95c2e4fbc16b6a7902053659fc88263d0a
Naod Hunde <chris_naod@yahoo.com>
2023-03-16 23:13:06 +1100

    comment.js file

---
+++ models/Comment.js
@@ -0,0 +1,53 @@
+// Import necessary packages and configuration
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Define Comment model by extending Sequelize's Model class
+class Comment extends Model {}
+
+// Initialize Comment model with columns and data types
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+ 
+    comment_description: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    
+    blog_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'blog',
+        key: 'id',
+      },
+    },
+
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+// Export Comment model
+module.exports = Comment;
