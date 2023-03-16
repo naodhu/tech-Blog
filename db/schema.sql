e5f2ee3c66266195d8bef74e6dabbfa8ba5f234a
Naod Hunde <chris_naod@yahoo.com>
2023-03-16 23:12:25 +1100

    schema files for tech_blog_db

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+--  The following SQL statements will drop the tech_blog_db database if it exists
+--  and then create a new tech_blog_db database.
+
+DROP DATABASE IF EXISTS tech_blog_db;
+CREATE DATABASE tech_blog_db;
