5a2a51fce715d72f6800c59fc051627b2e3a0a05
Naod Hunde <chris_naod@yahoo.com>
2023-03-23 11:20:40 +1100

    fixed minor error

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // Import required modules
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const helpers = require("./utils/helpers");
 
 // Import Sequelize and Sequelize Store to manage sessions
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 // Create Express.js application
 const app = express();
@@ -19,28 +19,29 @@ const hbs = exphbs.create({ helpers });
 
 // Set up session object
 const sess = {
-  secret: 'Super secret secret', // Secret used to sign session ID cookie
+  secret: "Super secret secret", // Secret used to sign session ID cookie
   cookie: {}, // Defaults to empty object, but can be customized
   resave: false, // Don't save session if nothing changed
   saveUninitialized: true, // Save uninitialized sessions
-  store: new SequelizeStore({ // Store sessions in database via Sequelize
-    db: sequelize
-  })
+  store: new SequelizeStore({
+    // Store sessions in database via Sequelize
+    db: sequelize,
+  }),
 };
 
 // Use session middleware
 app.use(session(sess));
 
 // Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // Parse incoming requests with JSON and urlencoded payloads
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Use routes defined in the controllers directory
 app.use(routes);
@@ -50,6 +51,9 @@ app.use(routes);
 //   app.listen(process.env.PORT || 3001, () => console.log('Now listening'));
 // });
 sequelize.sync({ force: false }).then(() => {
-  app.listen(process.env.PORT || 3001, () => console.log('Now listening on port ' + (process.env.PORT || 3001)));
+  app.listen(process.env.PORT || 3001, () =>
+    console.log("Now listening on port " + (process.env.PORT || 3001))
+  );
 });
+
 
